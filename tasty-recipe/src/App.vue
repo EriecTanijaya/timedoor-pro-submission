d9a913d4ee1c44d79bb9e310a0e7a7b0b1c8adf7
Eriec Tanijaya <eriectan88@gmail.com>
2025-08-27 14:16:05 +0700

    feat(tasty-recipe): initial commit

---
+++ tasty-recipe/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <web-header></web-header>
+    <home-page></home-page>
+    <web-footer></web-footer>
+  </div>
+</template>
+
+<script>
+  import WebFooter from "./components/footer/WebFooter.vue";
+import WebHeader from "./components/header/WebHeader.vue";
+import HomePage from "./components/pages/HomePage.vue";
+
+  export default {
+    components: {
+      HomePage,
+      WebHeader,
+      WebFooter
+    }
+  };
+</script>
